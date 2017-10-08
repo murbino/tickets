4192c71cc23ad8dcb619154c6bdecd99abb26c79
Manny Urbino <32224520+murbino@users.noreply.github.com>
2017-10-07 20:53:26 -0400

    Update comments

--- conference.sol
+++ conference.sol
@@ -1,6 +1,11 @@
 pragma solidity ^0.4.10;
 
-//Selling a fixed number of tickets 
+//Manny Urbino
+//October 7, 2017
+//Contract for a conference where registrants can buy tickets, and the
+//organizer can set a maximum quota of attendees as well as provide refunds.
+
+//Sell a fixed number of tickets 
 contract Conference{
 
   address public organizer;
@@ -12,7 +17,7 @@ contract Conference{
   event Deposit(address _from, uint _amount); 
   event Refund(address _to, uint _amount);
   
-  //Set limit and initialize count
+  //Set ticket limit and initialize count
   function Conference(){
     organizer = msg.sender;
     quota = 100;
@@ -30,7 +35,7 @@ contract Conference{
     return true;
   }
   
-  // Change limit
+  // Change ticket limit
   function changeQuota(uint newquota) public{
     if (msg.sender != organizer){
         return;
@@ -38,7 +43,7 @@ contract Conference{
     quota = newquota;
   }
   
-  //Refund tickets
+  //Refund Payment
   function refundTicket(address recipient, uint amount) public{
     if (msg.sender != organizer){
         return;
@@ -54,7 +59,7 @@ contract Conference{
     }
   }
   
-  //Send funds back to organizer
+  //Send funds back to organizer and make contract unusable
   function destroy(){
     if (msg.sender == organizer){
       suicide(organizer);
